7ba27cef769def7857581756fd1b9a20dc54167f
Hunter Praska <hunter@wiggin-labs.com>
2021-09-15 23:29:31 -0500

    Some work on generating ELF

--- elf/src/lib.rs
+++ elf/src/lib.rs
@@ -3,6 +3,7 @@ extern crate byteorder;
 
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 
+use std::mem;
 use std::collections::HashMap;
 
 pub struct Elf {
@@ -18,12 +19,14 @@ impl Elf {
     // data is a list of constants
     // rewrites is a map of indexes into the program to data indexes
     pub fn new(isa: ISA, mut program: Vec<u8>, data: Vec<Vec<u8>>, rewrites: HashMap<usize, usize>) -> Self {
-        let shstrtab = b"\0.text\0.data\0.shstrtab\0";
+        // TODO
+        assert!(program.len() < 0x200000);
         let data_offset = 64+56+56+program.len() as u64;
 
         // Read memory positions of each data entry
         let mut data_position = Vec::new();
         let mut pos = DATA_LOCATION + data_offset;
+        //let mut pos = DATA_LOCATION;
         let mut data_len = 0;
         for id in &data {
             data_position.push(pos);
@@ -47,20 +50,43 @@ impl Elf {
             }
         }
 
-        let p_hdr_size = data_offset;
-        let shstrtab_offset = data_offset + data_len as u64;
-        let sh_off = shstrtab_offset + shstrtab.len() as u64;
         Elf {
-            e_hdr: Elf64Ehdr::new(isa, sh_off),
-            p_hdr: vec![Elf64Phdr::text(0, p_hdr_size),
-                        Elf64Phdr::data(data_offset, data_len as u64)],
-            shstrtab: shstrtab.to_vec(),
-            s_hdr: vec![Elf64Shdr::null(), Elf64Shdr::text(program.len() as u64),
-                        Elf64Shdr::data(data_len as u64, data_offset),
-                        Elf64Shdr::shstrtab(shstrtab.len() as u64, shstrtab_offset)],
+            e_hdr: Elf64Ehdr::new(isa),
+            //p_hdr: vec![Elf64Phdr::text(0, program.len() as u64),
+            //            Elf64Phdr::data(0, data_len as u64)],
+            p_hdr: vec![Elf64Phdr::text(0, data_offset),
+                        //Elf64Phdr::data(data_offset, data_len as u64)],
+                        Elf64Phdr::data(0, data_len as u64)],
+            shstrtab: Vec::new(),
+            s_hdr: Vec::new(),
             data: data,
             program: program,
         }
+    }
+
+    pub fn new_debug(isa: ISA, program: Vec<u8>, data: Vec<Vec<u8>>, rewrites: HashMap<usize, usize>) -> Self {
+        let shstrtab = b"\0.text\0.data\0.shstrtab\0";
+        let data_offset = 64+56+56+program.len() as u64;
+        let mut data_len = 0;
+        for id in &data {
+            data_len += id.len();
+        }
+        let shstrtab_offset = data_offset + data_len as u64;
+        let sh_off = shstrtab_offset + shstrtab.len() as u64;
+
+        let s_hdr = vec![Elf64Shdr::null(),
+                        Elf64Shdr::text(program.len() as u64),
+                        Elf64Shdr::data(data_len as u64, data_offset),
+                        Elf64Shdr::shstrtab(shstrtab.len() as u64, shstrtab_offset)];
+
+        let mut elf = Self::new(isa, program, data, rewrites);
+        elf.e_hdr.e_shoff = sh_off;
+        elf.e_hdr.e_shnum = 4;
+        elf.e_hdr.e_shstrndx = 3;
+        elf.shstrtab = shstrtab.to_vec();
+        elf.s_hdr = s_hdr;
+
+        elf
     }
 
     pub fn to_vec(self) -> Vec<u8> {
@@ -90,6 +116,7 @@ type Elf64Word = u32;
 type Elf64Xword = u64;
 
 const EI_NIDENT: usize = 16; // Number of bytes in e_ident
+const ENTRY_LOCATION: u64 = 0x400000;
 const DATA_LOCATION: u64 = 0x600000;
 
 pub enum ISA {
@@ -97,6 +124,7 @@ pub enum ISA {
     Riscv = 0xf3,
 }
 
+#[repr(packed)]
 struct Elf64Ehdr {
     e_ident: [u8; EI_NIDENT],
     e_type: Elf64Half,
@@ -115,7 +143,8 @@ struct Elf64Ehdr {
 }
 
 impl Elf64Ehdr {
-    fn new(isa: ISA, sh_off: u64) -> Self {
+    //fn new(isa: ISA) -> Self {
+    fn new(isa: ISA) -> Self {
         Elf64Ehdr {
             e_ident: [0x7f, b'E', b'L', b'F', // magic number
                       2, // 1 for 32 bit, 2 for 64bit
@@ -130,26 +159,25 @@ impl Elf64Ehdr {
             // set to 1 for the original version of elf
             e_version: 1,
             // memory address of the entry point. right after elf header + program header
-            e_entry: 0x4000b0,
+            e_entry: ENTRY_LOCATION + 0xb0,
             // phoff: points to start of the program header table
-            e_phoff: 0x40,
+            e_phoff: mem::size_of::<Elf64Ehdr>() as u64,
             // shoff: points to start of the section header table
-            //e_shoff: 0x110,
-            e_shoff: sh_off,
+            e_shoff: 0,
             // flags
             e_flags: 0,
             // size of this header, usually 64 bytes on 64bit
-            e_ehsize: 64,
+            e_ehsize: mem::size_of::<Elf64Ehdr>() as u16,
             // size of a program header table entry
-            e_phentsize: 56,
+            e_phentsize: mem::size_of::<Elf64Phdr>() as u16,
             // number of entries in the program header table
             e_phnum: 2,
             // size of a section header table entry
-            e_shentsize: 64,
+            e_shentsize: mem::size_of::<Elf64Shdr>() as u16,
             // number of entries in the section header table
-            e_shnum: 4,
+            e_shnum: 0,
             // index of the section header table entry that contains the section names
-            e_shstrndx: 3,
+            e_shstrndx: 0,
         }
     }
 
@@ -173,6 +201,7 @@ impl Elf64Ehdr {
     }
 }
 
+#[repr(packed)]
 struct Elf64Phdr {
     p_type: Elf64Word,
     p_flags: Elf64Word,
@@ -185,7 +214,7 @@ struct Elf64Phdr {
 }
 
 impl Elf64Phdr {
-    fn text(offset: u64, size: u64,) -> Self {
+    fn text(offset: u64, size: u64) -> Self {
         Elf64Phdr {
             // 1 is PT_LOAD
             p_type: 1,
@@ -194,11 +223,11 @@ impl Elf64Phdr {
             // offset from beginning of segments
             p_offset: offset,
             // Initial virtual memory address to load this segment to
-            p_vaddr: 0x400000,
-            p_paddr: 0x400000,
+            p_vaddr: ENTRY_LOCATION,
+            p_paddr: ENTRY_LOCATION,
             p_filesz: size,
             p_memsz: size,
-            p_align: 0x200000,
+            p_align: 4096,
         }
     }
 
@@ -211,11 +240,11 @@ impl Elf64Phdr {
             // offset from beginning of segments
             p_offset: offset,
             // Initial virtual memory address to load this segment to
-            p_vaddr: 0x600000 + offset,
-            p_paddr: 0x600000 + offset,
+            p_vaddr: DATA_LOCATION,
+            p_paddr: DATA_LOCATION,
             p_filesz: size,
             p_memsz: size,
-            p_align: 0x200000,
+            p_align: 4096,
         }
     }
 
@@ -233,6 +262,7 @@ impl Elf64Phdr {
     }
 }
 
+#[repr(packed)]
 struct Elf64Shdr {
     sh_name: Elf64Word,
     sh_type: Elf64Word,
@@ -267,7 +297,7 @@ impl Elf64Shdr {
             sh_name: 0x01,
             sh_type: 1,
             sh_flags: 6,
-            sh_addr: 0x4000b0,
+            sh_addr: ENTRY_LOCATION + 0xb0,
             sh_offset: 0xb0,
             sh_size: sh_size,
             sh_link: 0,
@@ -320,5 +350,17 @@ impl Elf64Shdr {
         v.write_u64::<LittleEndian>(self.sh_addralign).unwrap();
         v.write_u64::<LittleEndian>(self.sh_entsize).unwrap();
         v
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn size() {
+        assert_eq!(mem::size_of::<Elf64Ehdr>(), 64);
+        assert_eq!(mem::size_of::<Elf64Phdr>(), 56);
+        assert_eq!(mem::size_of::<Elf64Shdr>(), 64);
     }
 }
